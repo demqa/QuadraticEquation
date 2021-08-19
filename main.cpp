82a61c1ba2c89f1b75767c73992762490410b087
demqa <33611332+demqa@users.noreply.github.com>
2021-08-19 16:46:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <math.h>
-using namespace std;
+#define EPS 1E-9
 
 void solveQuadraticEquation(double &a, double &b, double &c){
-    if(a == 0){
-        cout << "this is not quadratic equation";
+    if(abs(a)<EPS){
+        printf("this is not quadratic equation, linear solution: %.4f", -c/a);
         return;
     }
     double d = b*b-4*a*c;
     if(d < 0){
-        cout << "i have to solve this in complex form? for real?";
+        printf("equation has no roots");
     }
-    if(d == 0){
-        cout << "equation has the only root: " <<  -b/(2*a);
+    if(abs(d)<EPS){
+        printf("equation has the only root: %.4lf", (-b)/(2*a));
     }
     if(d > 0){
-        cout << "equation has two roots: " << (-b-sqrt(d))/(2*a) << " and " << (-b+sqrt(d))/(2*a);
+        printf("equation has two roots: %.4lf and %.4lf", (-b-sqrt(d))/(2*a), (-b+sqrt(d))/(2*a));
     }
 }
 
 int main() {
     double a,b,c;
-    cin >> a >> b >> c;
+    printf("enter 3 numbers. a,b,c - coefficients of quadratic equation\n");
+    scanf("%lf %lf %lf", &a, &b, &c);
     solveQuadraticEquation(a,b,c);
 }
+
