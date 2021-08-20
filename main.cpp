d6079279858cb32cee35090a9bca6178ed3d0707
demqa <33611332+demqa@users.noreply.github.com>
2021-08-21 00:08:59 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+
 #define EPS 1E-9
 
-void solveQuadraticEquation(double &a, double &b, double &c){
+void solveQuadraticEquation(double a, double b, double c){
     if(abs(a)<EPS){
-        printf("this is not quadratic equation, linear solution: %.4f", -c/b);
+        if(abs(b)<EPS){
+            if(abs(c)<EPS){
+                printf("every real number is a solution of this equation");
+            }
+            else{
+                printf("equation has no roots");
+            }
+        }
+        else{
+            printf("this is not quadratic equation, linear solution: %.4f", -c/b);
+        }
         return;
     }
     double d = b*b-4*a*c;
@@ -12,17 +23,16 @@ void solveQuadraticEquation(double &a, double &b, double &c){
         printf("equation has no roots");
     }
     if(abs(d)<EPS){
-        printf("equation has the only root: %.4lf", (-b)/(2*a));
+        printf("equation has the only root: %.4f", (-b)/(2*a));
     }
     if(d > 0){
-        printf("equation has two roots: %.4lf and %.4lf", (-b-sqrt(d))/(2*a), (-b+sqrt(d))/(2*a));
+        printf("equation has two roots: %.4f and %.4f", (-b-sqrt(d))/(2*a), (-b+sqrt(d))/(2*a));
     }
 }
 
-int main() {
+int main(){
     double a,b,c;
     printf("enter 3 numbers. a,b,c - coefficients of quadratic equation\n");
     scanf("%lf %lf %lf", &a, &b, &c);
     solveQuadraticEquation(a,b,c);
 }
-
