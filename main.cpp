4eae420f5bfefd59ef91cea22c51852e226b0e1e
demqa <33611332+demqa@users.noreply.github.com>
2021-08-21 01:59:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,20 @@
 #include <stdio.h>
-#include <math.h>
-
-#define EPS 1E-9
-
-void solveQuadraticEquation(double a, double b, double c){
-    if(abs(a)<EPS){
-        if(abs(b)<EPS){
-            if(abs(c)<EPS){
-                printf("every real number is a solution of this equation");
-            }
-            else{
-                printf("equation has no roots");
-            }
-        }
-        else{
-            printf("this is not quadratic equation, linear solution: %.4f", -c/b);
-        }
-        return;
-    }
-    double d = b*b-4*a*c;
-    if(d < 0){
-        printf("equation has no roots");
-    }
-    if(abs(d)<EPS){
-        printf("equation has the only root: %.4f", (-b)/(2*a));
-    }
-    if(d > 0){
-        printf("equation has two roots: %.4f and %.4f", (-b-sqrt(d))/(2*a), (-b+sqrt(d))/(2*a));
-    }
-}
+#include "qe.h"
 
 int main(){
-    double a,b,c;
+    double a = 0, b = 0, c = 0;
     printf("enter 3 numbers. a,b,c - coefficients of quadratic equation\n");
     scanf("%lf %lf %lf", &a, &b, &c);
-    solveQuadraticEquation(a,b,c);
+    double x1,x2;
+    int ans;
+    SolveQuadraticEquation(a,b,c,&x1,&x2,&ans);
+    if(ans == 0){
+        printf("equation has no roots\n");
+    }else if(ans == 1){
+        printf("equation has the only root: %.4f\n", x1);
+    }else if(ans == 2){
+        printf("equation has two roots: %.4f and %.4f\n", x1, x2);
+    }else if(ans == 3){
+        printf("every real number is a solution of this equation\n");
+    }
 }
