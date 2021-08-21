d59774ae8fcb2fb3883d32b5e0e6947bd91504df
demqa <33611332+demqa@users.noreply.github.com>
2021-08-21 14:03:20 +0300

    Update main.c
    undefined variables are now defined. construction (if else if else if) changed to switch.

--- main.c
+++ main.c
@@ -3,18 +3,26 @@
 
 int main(){
     double a = 0, b = 0, c = 0;
-    double x1,x2;
-    int ans;
+    double x1 = 0,x2 = 0;
+    int ans = -1;
     printf("enter 3 numbers. a,b,c - coefficients of quadratic equation\n");
     scanf("%lf %lf %lf", &a, &b, &c);
     SolveQuadraticEquation(a,b,c,&x1,&x2,&ans);
-    if(ans == 0){
-        printf("equation has no roots\n");
-    }else if(ans == 1){
-        printf("equation has the only root: %.4f\n", x1);
-    }else if(ans == 2){
-        printf("equation has two roots: %.4f and %.4f\n", x1, x2);
-    }else if(ans == 3){
-        printf("every real number is a solution of this equation\n");
+    switch(ans){
+        case 0:
+            printf("equation has no roots\n");
+            break;
+        case 1:
+            printf("equation has the only root: %.4f\n", x1);
+            break;
+        case 2:
+            printf("equation has two roots: %.4f and %.4f\n", x1, x2);
+            break;
+        case 3:
+            printf("every real number is a solution of this equation\n");
+            break;
+        default:
+            printf("there is some error\n");
+            break;
     }
 }
