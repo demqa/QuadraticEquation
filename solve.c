e7e0e6c4eadbce0a8b5d077801d04819a07778bc
demqa <33611332+demqa@users.noreply.github.com>
2021-08-21 14:01:06 +0300

    Update solve.c
    added function that compare number with zero and if it close to zero changes value to exactly zero. removed useless header. #define EPS moved to static const EPS.

--- solve.c
+++ solve.c
@@ -1,30 +1,15 @@
 #include <math.h>
-#include "qe.h"
 
-void SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2, int *ans){
-    if(fabs(a)<EPS){
-        SolveLinearEquation(b,c,x1,ans);
-        return;
-    }
-    double d = b*b - 4*a*c;
-    if(d < 0){
-        *ans = 0;
-    }
-    if(fabs(d)<EPS){
-        *ans = 1;
-        *x1 = -b/(2*a);
-    }
-    if(d > 0){
-        *ans = 2;
-        *x1 = (-b - sqrt(d))/(2*a);
-        *x2 = (-b + sqrt(d))/(2*a);
+void CheckZero(double *a){
+    static const double EPS = 1E-9;
+    if(fabs(*a)<EPS){
+        *a = 0.0;
     }
 }
 
-
 void SolveLinearEquation(double b, double c, double *x1, int *ans){
-    if(fabs(b)<EPS){
-        if(fabs(c)<EPS){
+    if(b == 0.0){
+        if(c == 0.0){
             *ans = 3;
         }
         else{
@@ -34,6 +19,30 @@ void SolveLinearEquation(double b, double c, double *x1, int *ans){
     else{
         *ans = 1;
         *x1 = -c/b;
+    }
+}
+
+void SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2, int *ans){
+    CheckZero(&a);
+    CheckZero(&b);
+    CheckZero(&c);
+    if(a == 0.0){
+        SolveLinearEquation(b,c,x1,ans);
+        return;
+    }
+    double d = b*b - 4*a*c;
+    CheckZero(&d);
+    if(d < 0){
+        *ans = 0;
+    }
+    if(d == 0.0){
+        *ans = 1;
+        *x1 = -b/(2*a);
+    }
+    if(d > 0){
+        *ans = 2;
+        *x1 = (-b - sqrt(d))/(2*a);
+        *x2 = (-b + sqrt(d))/(2*a);
     }
 }
 
