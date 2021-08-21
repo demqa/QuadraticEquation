1b6d8469b118a124510f33c90d0de31168e00bf2
demqa <33611332+demqa@users.noreply.github.com>
2021-08-21 14:04:44 +0300

    Update qe.h
    #define EPS moved to static const EPS in solve.c. void SolveLinearEquation is useless in main.c, then removed.

--- qe.h
+++ qe.h
@@ -1,3 +1,1 @@
-#define EPS 1E-9
 void SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2, int *solution);
-void SolveLinearEquation(double b, double c, double *x1, int *solution);
