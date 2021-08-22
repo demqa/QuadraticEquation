8b525267f453fba247e8aa7dbc7d61de4d0b4f63
demqa <33611332+demqa@users.noreply.github.com>
2021-08-22 17:25:50 +0300

    renamed and updated

---
+++ QEquation.h
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2);
+bool Equal(double a, double b);
+bool TestManager();
+
+#define ANY_VALUE 3
--- qe.h
+++
@@ -1,3 +0,0 @@
-#include <stdbool.h>
-int SolveQuadraticEquation(double a, double b, double c, double *x1, double *x2);
-bool TestManager();
